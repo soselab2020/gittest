cfc7c673c638e1de0a7b105f0c2f41c54d516f0b
Albert Ma <shangpinma@gmail.com>
2024-10-30 11:10:34 +0800

    fix: add cow

--- AnimalTest.java
+++ AnimalTest.java
@@ -8,6 +8,10 @@ public class AnimalTest {
 		Cat cat = new Cat(); // 若換成Cat dog = new Cat(); 結果就不一樣
 		demo.lyric(cat);		
 		demo.lyric(dog + "\n" + cat);
+
+		Cow cow = new Cow(); 	
+		demo.lyric(cow);		
+		
 	}
 }
 
